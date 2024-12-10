e311fe4547c1929ab7726f0b48ec48b49f13e82c
PSlobodskoy <PSlobodskoy@one.kz>
2024-12-10 08:19:09 +0300

    sm fix

--- src/front/_idb/complect.idb.ts
+++ src/front/_idb/complect.idb.ts
@@ -58,21 +58,3 @@ class ComplectIDB extends DexieDB<Storage> {
 export const complectIDB = new ComplectIDB();
 
 export const authSignal = createDexieSignalQuery(() => complectIDB.getSingleValue('auth'));
-
-// complectIDB.setSingleValue('auth', {
-//   fio: 'Павел 👨‍💻',
-//   level: 100,
-//   login: 'T166cbf17596a0c2919e4df1a6d54e66',
-//   nick: 'gfirf',
-//   passw: 'ddc314998f6e2dbafd0167890d3edd76',
-//   tgAva: 'https://t.me/i/userpic/320/O7wMFN1MteRncm1KQavdO1Zk8kEaJsvO2WaCcyZPQbo.jpg',
-//   tgId: 958383738,
-// });
-
-// complectIDB.setSingleValue('auth', {
-//   fio: 'User',
-//   level: 3,
-//   login: '8f9bfe9d1345237cb3b2b205864da075',
-//   nick: 'user',
-//   passw: '731bf54651ae87d30d82ab34559947bd',
-// });
